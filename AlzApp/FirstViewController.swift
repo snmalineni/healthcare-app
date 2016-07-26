6fceb61246e9ecad211243fcfa78bace4a859012
Sneha Malineni <sneha@Snehas-MacBook-Air.local>
2016-07-26 13:13:01 -0700

    New and updated shell

---
+++ AlzApp/FirstViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FirstViewController.swift
+//  AlzApp
+//
+//  Created by Sneha Malineni on 7/25/16.
+//  Copyright © 2016 Sneha Malineni. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
---
+++ AlzApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  AlzApp
+//
+//  Created by Sneha Malineni on 7/25/16.
+//  Copyright © 2016 Sneha Malineni. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
